5f8c6cd28827d04fa64b5c57c394dcc13f1028d2
Davesky <fractal@maui.net>
2017-02-20 17:31:41 -1000

    Add Push Notifications

--- BadButton/AppDelegate.m
+++ BadButton/AppDelegate.m
@@ -5,6 +5,7 @@
 //  Created by Dave Scruton on 2/16/17.
 //  Copyright © 2017 Huedoku Labs. All rights reserved.
 //
+//  DHS 2/20 Added RemoteNotifications hookup / delegate callbacks
 
 #import "AppDelegate.h"
 
@@ -17,6 +18,21 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    NSLog(@" didFinishLaunchingWithOptions... register for remote notifications...");
+    if ([application respondsToSelector:@selector(registerUserNotificationSettings:)])
+    {
+        UIUserNotificationType types = (UIUserNotificationTypeAlert | UIUserNotificationTypeBadge | UIUserNotificationTypeSound);
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+        [application registerUserNotificationSettings:settings];
+        [application registerForRemoteNotifications];
+    }
+    else
+    {
+        [application registerForRemoteNotificationTypes:(UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)];
+    }
+    
+
     return YES;
 }
 
@@ -45,6 +61,77 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+
+
+
+#pragma mark UIApplicationDelegate ....
+
+//=======<SSD App Delegate>===================================================
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+
+    
+    NSLog(@" didRegisterForRemoteNotificationsWithDeviceToken %@",deviceToken);
+
+    NSString *title = @"AppDelegate...";
+    NSString *message  = [NSString stringWithFormat:@" didRegisterForRemoteNotificationsWithDeviceToken %@",deviceToken];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+                                                    message:message
+                                                   delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    [alert show];
+
+#ifdef PARSE_NOTIFICATIONS_ENABLED
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    [currentInstallation setDeviceTokenFromData:deviceToken];
+    [currentInstallation saveInBackground];
+    
+    [PFPush subscribeToChannelInBackground:@"" block:^(BOOL succeeded, NSError *error) {
+        if (succeeded) {
+            //NSLog(@"ParseStarterProject successfully subscribed to push notifications on the broadcast channel.");
+        } else {
+            //NSLog(@"ParseStarterProject failed to subscribe to push notifications on the broadcast channel.");
+        }
+    }];
+#endif
+    
+}
+
+//=======<SSD App Delegate>===================================================
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    NSLog(@" didFailToRegisterForRemoteNotificationsWithError %@",error);
+    NSString *title = @"AppDelegate...";
+    NSString *message  = [NSString stringWithFormat:@" didFailToRegisterForRemoteNotificationsWithError %@ : %d",error,error.code];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+                                                    message:message
+                                                   delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    [alert show];
+    if (error.code == 3010) {
+        //NSLog(@"Push notifications are not supported in the iOS Simulator.");
+    } else {
+        // show some alert or otherwise handle the failure to register.
+        //NSLog(@"application:didFailToRegisterForRemoteNotificationsWithError: %@", error);
+    }
+}
+
+//=======<SSD App Delegate>===================================================
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    NSLog(@" didReceiveRemoteNotification %@",userInfo);
+    NSString *title = @"AppDelegate...";
+    NSString *message  = [NSString stringWithFormat:@" didReceiveRemoteNotification %@",userInfo];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+                                                    message:message
+                                                   delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    [alert show];
+#ifdef PARSE_NOTIFICATIONS_ENABLED
+    [PFPush handlePush:userInfo];
+    
+    if (application.applicationState == UIApplicationStateInactive) {
+        [PFAnalytics trackAppOpenedWithRemoteNotificationPayload:userInfo];
+    }
+#endif
 }
 
 
